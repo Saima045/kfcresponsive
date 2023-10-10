26110590c5c44b758e9c4410e96ee50c71c00042
saima <saima045@gmail.com>
2023-10-10 13:08:27 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//alert('test');
+let icon=document.querySelector('.bi-list');
+let sidebar=document.querySelector('.sidebar')
+let overlay=document.querySelector('.overlay')
+let buttonSide=document.querySelectorAll('.btnList')
+//alert('youuuuu')
+//console.log(icon)
+icon.addEventListener('click',()=>{
+   
+ //alert('immmm')
+    sidebar.style.transform='translate(0)'
+    overlay.style.transform='translate(0)'
+    
+})
+
+ overlay.addEventListener('click',()=>{
+    
+    sidebar.style.transform='translate(-100%)'
+    overlay.style.transform='translate(-100%)'
+ })
+
+ buttonSide.addEventListener('mouseover',()=>{
+    alert('im here')
+ })
